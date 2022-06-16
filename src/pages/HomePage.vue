021988133f2e4c02d3b62bc6cbbb9ef9d29b4dbe
Annika <nikzneko@gmail.com>
2022-06-16 15:49:46 -0600

    creating homepage... slowly... unsteadily...

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo" class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
-  </div>
+  <post v-for="p in AppState.posts" :key="p.id" :post="p" />
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  setup() {
+
+    return {
+      
+    }
+  }
 }
 </script>
 
